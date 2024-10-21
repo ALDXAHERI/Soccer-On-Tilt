6098dc7d413081a510e1ed287f0dfad537b6e3b0
ALDXAHERI <148928603+ALDXAHERI@users.noreply.github.com>
2024-10-21 18:58:50 -0400

    finished player movement

--- Soccer On Tilt/Assets/Scripts/PlayerMovement.cs
+++ Soccer On Tilt/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+    public bool isGrounded;
+
+    private Rigidbody2D rb;
+    public bool isPlayerOne;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerOne)
+        {
+            HandlePlayerOneInput();
+        }
+        else
+        {
+            HandlePlayerTwoInput();
+        }
+    }
+
+    void HandlePlayerOneInput()
+    {
+        float move = 0;
+        if (Input.GetKey(KeyCode.A)) move = -1;
+        if (Input.GetKey(KeyCode.D)) move = 1;
+
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void HandlePlayerTwoInput()
+    {
+        float move = 0;
+        if (Input.GetKey(KeyCode.LeftArrow)) move = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) move = 1;
+
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
     }
 }
